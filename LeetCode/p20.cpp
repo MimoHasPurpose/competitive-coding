75a31082f260eb9c61e2a29dc70a4d6903ab7d33
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-04-29 16:19:18 +0530

    problem 20

---
+++ LeetCode/p20.cpp
@@ -0,0 +1,20 @@
+// Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+// Return the largest lucky integer in the array. If there is no lucky integer return -1.
+
+
+
+class Solution {
+public:
+    int findLucky(vector<int>& arr)
+    {
+        map<int,int>map;
+      for(int i=0;i<arr.size();i++){
+          map[arr[i]]++;
+      }
+      for(auto it=map.rbegin();it!=map.rend();it++){
+         if(it->first==it->second)return it->first; 
+      }
+     return -1;
+    }
+};
