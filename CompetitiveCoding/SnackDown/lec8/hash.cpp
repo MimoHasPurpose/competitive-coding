402b17389ecba0f2d432e0be37217faa779cd707
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-06-10 17:41:03 +0530

    string lecture questions

--- CompetitiveCoding/SnackDown/lec8/hash.cpp
+++ CompetitiveCoding/SnackDown/lec8/hash.cpp
@@ -1,1 +1,36 @@
-// string lecture 2nd code
+// string hashing
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int calHash(string s)
+{
+    const int p = 31;
+    const long long m = 1e9 + 9;
+    long long hash = 0;
+    long long x = 1;
+    for (int i = 0; i < s.size(); i++)
+    {
+        // formula - multiply s[i]*p^i;
+        hash = ((hash) + ((s[i] - 'a' + 1) * x)) % m;
+        x = (x * p) % m;
+    }
+    return hash;
+}
+int main()
+{
+    string A, B;
+    cin >> A >> B;
+    if (calHash(A) == calHash(B))
+    {
+        cout << "Equal! " << endl;
+    }
+    else
+    {
+        cout << "Not Equal! " << endl;
+    }
+    cout << calHash("umang") << endl;
+    cout << calHash("umansg") << "\n";
+
+    return 0;
+}
--- CompetitiveCoding/SnackDown/lec8/p1.cpp
+++ CompetitiveCoding/SnackDown/lec8/p1.cpp
@@ -31,6 +31,40 @@ vector<int> calPrefix(string pat, int N)
     }
     return prefix;
 }
+bool KMP(string txt, string pat, vector<int> &pre, int N, int M)
+{
+    int i = 0;
+    int j = 0;
+    // i will move forward in text and j in pattern.
+    while (i < N)
+    {
+        if (j == M)
+        {
+            return true;
+        }
+        // i =N-1,j=M-1, they are equal
+        // i =N, j=M
+        if (txt[i] == pat[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            // txt pointers -i that will move forward
+            // pat pointer -j  will move back to pre[j-1]
+            if (j != 0)
+            {
+                j = pre[j - 1];
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+    return false;
+}
 int main()
 {
     string txt, pat;
@@ -41,6 +75,15 @@ int main()
     for (int i = 0; i < M; i++)
     {
         cout << prefix[i] << " ";
+    }
+    cout << endl;
+    if (KMP(txt, pat, prefix, N, M))
+    {
+        cout << "Pattern found" << endl;
+    }
+    else
+    {
+        cout << "Not found!" << endl;
     }
     return 0;
 }
