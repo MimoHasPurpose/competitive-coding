90f9807d53b7e89963d389f40b552a7470b7e568
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-03 10:23:31 +0530

    update

--- CompetitiveCoding/e1.cpp
+++ CompetitiveCoding/e1.cpp
@@ -2,7 +2,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<'c'+1<<endl;
-    cout<<3/2.0<<endl;
-    
+//    cout<<'c'+1<<endl;
+//    cout<<3/2.0<<endl;
+//    int mx=INT_MAX+1;
+//    cout<<mx;
+//    int a=100000;
+//    int b=100000;
+//	long int c=a*1LL*b;
+//    cout<<c<<endl;
+	double a=100000;
+	double b=100000;
+	double c=a*b;
+	c=1e24;
+	cout<<fixed<<c<<endl;	
 }
