fa4b066ce7b810f8e10a7e32d273d5ccccbc3b48
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-04 09:16:43 +0530

    update

---
+++ CompetitiveCoding/e3.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+//  break: comes out of last for loop
+// 	continue: last for loop continued
+	int a;
+	while(true)
+	{
+		cin>>a;
+		if(a==42)
+		break;
+		else	
+		cout<<a;
+}
+
+
+	
+	
+}
