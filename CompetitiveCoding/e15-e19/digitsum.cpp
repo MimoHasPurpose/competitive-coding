c8f6b467428f33eb717d7e67388fec227f00d753
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-14 10:03:07 +0530

    update

--- CompetitiveCoding/e15-e19/digitsum.cpp
+++ CompetitiveCoding/e15-e19/digitsum.cpp
@@ -1,20 +1,15 @@
-// sum of array
-// sum(n,a)-> sum of elements uptill n index
-// sum(n,a)=a[n]+sum(n-1,a)
+
 #include<bits/stdc++.h>
 using namespace std;
-int sum(int n,int a[]){
-    if(n<0) return 0;
-    return sum(n-1,a)+a[n];
+int digitsum(int n){
+    if(n==0) return 0;
+    return digitsum(n/10)+(n%10);
 }
 
 int main()
 {
     int n;
     cin>>n;
-    int a[n];
-    for(int i=0;i<n;++i){
-        cin>>a[i];
-    }
-    cout<<sum(n-1,a);
+ 
+    cout<<digitsum(n);
 }
