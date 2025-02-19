b927c01ba23307cf6b50a14fb97d378109535cfa
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-19 10:19:27 +0530

    auto and range

--- CompetitiveCoding/e20-e25/e22.cpp
+++ CompetitiveCoding/e20-e25/e22.cpp
@@ -4,11 +4,23 @@ int main()
 {
     vector<int> v={2,3,4,5,6};
     for(int i=0;i<v.size();++i){
-        cout<<v[i]<<" ";
+        // cout<<v[i]<<" ";
     }
     cout<<endl;
     vector<int> ::iterator it=v.begin();
     for(it=v.begin();it!=v.end();++it){
-        cout<<(*it)<<endl;
+        // cout<<(*it)<<endl;
     }
+
+    // range based loops
+    for(int value:v){
+        // cout<<value<<" ";
+    }
+    for(auto &value:v){
+        value++;
+        cout<<value<<" ";
+    }
+    auto a="sanjay";
+    cout<<a<<endl;
+    auto its=v.begin();
 }
