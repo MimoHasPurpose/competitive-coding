fa485a88662bb5ce95c7d3d1c70c6bd31bf99478
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-20 07:01:28 +0530

    unordered maps used for speed.

--- CompetitiveCoding/e20-e25/unorderedmaps.cpp
+++ CompetitiveCoding/e20-e25/unorderedmaps.cpp
@@ -1,8 +1,30 @@
+// Given N strings and Q queries.
+//  if each query we are given a string 
+// print frequency of that string.
+// unordered_map<pair<int,int>,string>m;
+// m[1]="abc";
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    unordered_map<int,string> m;
-    m[1]="abc";
-    
+    unordered_map<string,int> m;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        m[s]++;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        string s;
+        cin>>s;
+        cout<<m[s]<<endl;
+    }
+
+
+
+    // 
+
 }
