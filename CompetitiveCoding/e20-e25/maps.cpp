24cb88c9c407b00189025803e5606fc8401dcd6e
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-20 05:53:51 +0530

    update

--- CompetitiveCoding/e20-e25/maps.cpp
+++ CompetitiveCoding/e20-e25/maps.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 int main()
 {
-    
+    map<int,string> m;
+    m[1]="abc"; // insertion O(log(n))
+    m[5]="cde";
+    m[3]="acd";
+    m.insert({4,"afg"});
+    m[6]; // empty value inserted.
+    // can add same value again.
+    map<int,string>:: iterator it;
+    for(it =m.begin(); it!=m.end();++it){
+        cout<<(*it).first<<" "<<(*it).second<<endl;
+    }
+    for (auto &pr :m)
+    {
+        cout<<pr.first<<" "<<pr.second<<endl;
+    }
 }
