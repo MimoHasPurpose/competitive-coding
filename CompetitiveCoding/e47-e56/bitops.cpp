0a9b4fc6f47a04eebecfdff8d21ca2e50846377a
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-21 09:23:28 +0530

    update

--- CompetitiveCoding/e47-e56/bitops.cpp
+++ CompetitiveCoding/e47-e56/bitops.cpp
@@ -23,5 +23,26 @@ int main()
     // set bit
     printb(a| (1<<1));
     // unset bit
+    printb(~a);
+    printb(1<<3);
+    printb(~(1<<3));
+    printb(a&(~(1<<3)));
+
+    // toggle bit:
+    //toggle 2nd bit
+    printb(a^(1<<2));
+    printb(a^(1<<3));
     
+    //setbit count
+    printb(a);
+    int ct=0;
+    for(int i=31;i>=0;--i){
+        if((a&(1<<i))!=0){
+            ct++;
+        }
+    }
+    cout<<ct<<endl;
+    cout<<__builtin_popcount(a)<<endl;
+    cout<<__builtin_popcountll((1LL<<35)-1)<<endl;
+
 }
