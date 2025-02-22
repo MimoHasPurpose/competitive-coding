ca9fb2f8d15ab2e99869d0440306bc9f9a7b2558
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-22 09:50:41 +0530

    update

--- CompetitiveCoding/e47-e56/bittricks.cpp
+++ CompetitiveCoding/e47-e56/bittricks.cpp
@@ -18,18 +18,23 @@ int main()
     //         cout<<" Even"<<endl;
     //     }
     // }
-    for(char c='A';c<='D';++c){
-        cout<<c<<" ";
-        cout<<int(c)<<" ";
-        printb(int(c));
-    }
-    for(char c='a';c<='d';++c){
-        cout<<c<<" ";
-        cout<<int(c)<<" ";
-        printb(int(c));
-    }
-    char A='A';
-    char a=A|(1<<5);
-    cout<<A<<a<<endl;
-    
+    // for(char c='A';c<='D';++c){
+    //     cout<<c<<" ";
+    //     cout<<int(c)<<" ";
+    //     printb(int(c));
+    // }
+    // for(char c='a';c<='d';++c){
+    //     cout<<c<<" ";
+    //     cout<<int(c)<<" ";
+    //     printb(int(c));
+    // }
+    // char A='A';
+    // char a=A|(1<<5);
+    // cout<<A<<a<<endl;
+    // cout<<char((a&(~(1<<5))))<<endl;
+    printb(59);                                                                                                                                                                                                                                 
+    int a=59;
+    int i=4;
+    int b=(a&(~((1<<(i+1))-1)));
+    printb(b);
 }
