625ecb3c0b142a721c3b814ce1ae6b5e8f2cbf89
Sanjay Singh <sanjaygurjar741@gmail.com>
2025-02-22 09:24:31 +0530

    update

--- CompetitiveCoding/e47-e56/bittricks.cpp
+++ CompetitiveCoding/e47-e56/bittricks.cpp
@@ -1,7 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+void printb(int num){
+    for(int i=10;i>=0;--i){
+        cout<<((num>>i)&1);
+    }
+    // cout<<endl;
+ }
 int main()
 {
+    for(int i=0;i<8;++i){
+        printb(i);
+        if(i&1){
+            cout<<" Odd"<<endl;
 
+        }
+        else{
+            cout<<" Even"<<endl;
+        }
+    }
     
 }
