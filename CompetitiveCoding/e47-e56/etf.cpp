ec98e4a6232f43f89ef4b02cfb5babdf6dfc604d
Sanjay Singh <77540699+Sanj-bot@users.noreply.github.com>
2025-03-02 06:40:12 +0530

    update

---
+++ CompetitiveCoding/e47-e56/etf.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+const inst M=1e9+7;
+using namespace std;
+
+
+int main()
+{
+
+
+}
---
+++ CompetitiveCoding/e47-e56/largeexpo.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int M=1e9+7;
+
+int binMultiply(long long a,long long b){
+    int ans=0;
+    while(b>0){
+        if(b&1){
+            ans=(ans+a)%M;
+        }
+        a=(a+a)%M;
+        b>>=1;
+    }
+    return ans;
+}
+
+int main()
+{
+    cout<<binMultiply(10,5)<<endl;
+
+}
