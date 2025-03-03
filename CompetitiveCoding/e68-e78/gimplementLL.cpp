bad746be15bdd28011c743fa00b00a94808fbdfe
Sanjay Singh <77540699+Sanj-bot@users.noreply.github.com>
2025-03-03 16:37:33 +0530

    up

---
+++ CompetitiveCoding/e68-e78/gimplementLL.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+// implementing basic graph storage.
+using namespace std;
+const int N=1e3+10;
+int graph[N][N];
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;++i){
+        int v1,v2;
+        cin>>v1>>v2;
+        graph[v1][v2]=1;
+        graph[v2][v1]=1;
+    }
+    for(int i=0;i<m;++i)
+
+}
+
