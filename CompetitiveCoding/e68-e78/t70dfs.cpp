588f666822cf6736be1e3407f354caaf09b9c804
Sanjay Singh <77540699+Sanj-bot@users.noreply.github.com>
2025-03-04 11:15:21 +0530

    update

--- CompetitiveCoding/e68-e78/t70dfs.cpp
+++ CompetitiveCoding/e68-e78/t70dfs.cpp
@@ -5,9 +5,20 @@ vector<int> g[N];
 bool vis[N];
 
 void dfs(int vertex){
+    //take action on vertex after entering the vertex.
+    cout<<vertex<<endl;
+    vis[vertex]=true;
     for(int child:g[vertex]){
+        cout<<"par "<<vertex<<", child"<<child<<endl;
+        if(vis[child]) continue;
+        //take action on child before entering the child node.
         dfs(child);
+        //take action on child after exiting child node.
+
     }
+        //take action on vertex before exiting the vertex.
+
+    // return isLoopExists;
 }
 
 int main()
@@ -20,4 +31,6 @@ int main()
         g[v1].push_back(v2);
         g[v2].push_back(v1);
     }    
+     dfs(1);
+    return 0;
 }
