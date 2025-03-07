2ed5973608d509cadfa69058523af6796da03594
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-07 08:00:07 +0530

    UP

--- CompetitiveCoding/e68-e78/cycledetection.cpp
+++ CompetitiveCoding/e68-e78/cycledetection.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 const int N=1e5+10;
 vector<int> g[N];
-vector<vector<int>>cc;
-vector<int>current_cc;
 bool vis[N];
 
-void dfs(int vertex,int parent){
+bool dfs(int vertex,int par){
     //take action on vertex after entering the vertex.
     // cout<<vertex<<endl;
     
     vis[vertex]=true;
-    current_cc.push_back(vertex);
+    bool isLoopExists=false;
+
     for(int child:g[vertex]){
         // cout<<"par "<<vertex<<", child"<<child<<endl;
-        if(vis[child]) continue;
+        if(vis[child]&&child==par) continue;
+        if(vis[child])  return true;
 
         //take action on child before entering the child node.
-        dfs(child);
-
+        
+        isLoopExists|=dfs(child,vertex); //if one dfs return true than whole becomes true.
         //take action on child after exiting child node.
 
     }
         //take action on vertex before exiting the vertex.
 
-    // return isLoopExists;
+    return isLoopExists;
 }
 
 int main()
@@ -38,21 +38,16 @@ int main()
         g[x].push_back(y);
         g[y].push_back(x);
     }    
-     int ct=0;
+    bool isLoopExists=false;
+     
      for(int i=1;i<=n;++i){
         if(vis[i])continue;
-        current_cc.clear();
-        dfs(i);
-        cc.push_back(current_cc);
-        ct++;
-     }
-     cout<<endl;
-     cout<<cc.size()<<endl;
-     for(auto c_cc:cc){
-        for(int vertex:c_cc){
-            cout<<vertex<<" ";
+        if(dfs(i,0)){
+            isLoopExists=true;
+            break;
         }
-        cout<<endl;
      }
-    return 0;
+
+     cout<<isLoopExists<<endl;
+   
 }
