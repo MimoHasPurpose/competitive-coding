1d7a724c7d911b106931b75e80c20fcf0b967525
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-08 07:55:50 +0530

    precomputation in a tree

--- CompetitiveCoding/e68-e78/subtree.cpp
+++ CompetitiveCoding/e68-e78/subtree.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 const int N=1e5+10;
 vector<int> g[N];
+int subtree_sum[N];
+int even_count[N];
+int val[N];
 
 void dfs(int vertex,int par=0){
     //take action on vertex after entering the vertex.
     
-    
+    subtree_sum[vertex]+=vertex;
+    if(vertex%2==0)even_count[vertex]++;
     for(int child:g[vertex]){
     
         if(child==par) continue;
@@ -15,6 +19,8 @@ void dfs(int vertex,int par=0){
         dfs(child,vertex);
     
         //take action on child after exiting child node.
+        subtree_sum[vertex]+=subtree_sum[child];
+        even_count[vertex]+=even_count[child];
 
     }
         //take action on vertex before exiting the vertex.
@@ -33,6 +39,15 @@ int main()
         g[v2].push_back(v1);
     }    
     int q;
+    dfs(1);
+    for(int i=1;i<=n;i++){
+        cout<<subtree_sum[i]<<" "<<even_count[i]<<endl;
+    }
+    // while(q--){
+    //     int v;
+    //     cin>>v;
+        
+    // }
 
 
 }
