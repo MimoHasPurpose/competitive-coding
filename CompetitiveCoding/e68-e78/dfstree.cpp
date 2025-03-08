7459b2e059865804912a2accf3696bdfabc79041
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-08 07:30:36 +0530

    up

--- CompetitiveCoding/e68-e78/dfstree.cpp
+++ CompetitiveCoding/e68-e78/dfstree.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 const int N=1e5+10;
 vector<int> g[N];
-bool vis[N];
-
-void dfs(int vertex){
+int depth[N],height[N];
+void dfs(int vertex,int par=0){
     //take action on vertex after entering the vertex.
     cout<<vertex<<endl;
-    vis[vertex]=true;
+    
     for(int child:g[vertex]){
         cout<<"par "<<vertex<<", child"<<child<<endl;
-        if(vis[child]) continue;
+        if(child==par) continue;
         //take action on child before entering the child node.
-        dfs(child);
+        depth[child]=depth[vertex]+1;
+        dfs(child,vertex);
+        height[vertex]=max(height[vertex],height[child]+1);
         //take action on child after exiting child node.
 
     }
@@ -25,12 +26,15 @@ int main()
 {
     int n,m;
     cin>>n>>m;
-    for(int i=0;i<9;++i){
+    for(int i=0;i<n-1;++i){
         int v1,v2;
         cin>>v1>>v2;
         g[v1].push_back(v2);
         g[v2].push_back(v1);
     }    
      dfs(1);
+     for(int i=1;i<=n;++i){
+        cout<<depth[i]<<" "<<height[i]<<endl;
+     }
     return 0;
 }
