69f07ba5ea39dfae62a0eeecccb767e272a3d1ca
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-18 09:58:31 +0530

    nesting of maps

--- CompetitiveCoding/e27-e35/nestingInMapsSets.cpp
+++ CompetitiveCoding/e27-e35/nestingInMapsSets.cpp
@@ -2,4 +2,33 @@
 using namespace std;
 int main()
 {
+    // map<pair<int,int>,int>m;
+    map<pair<string,string>,vector<int>> students;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        string fn,ln;
+        int ct;
+        cin>>fn>>ln>>ct;
+        for(int i=0;i<ct;++i){
+            int x;
+            cin>>x;
+            students[{fn,ln}].push_back(x);
+        }
+    }
+
+    for(auto pr:students){
+        auto &full_name =pr.first;
+        auto &list=pr.second;
+        cout<<full_name.first<<" " <<full_name.second<<endl;
+        cout<<list.size()<<endl;
+        for(auto &element:list){
+            cout<<element<<" ";
+        }
+        cout<<endl;
+    }
+    pair<int,int> p1,p2;
+    p1={1,2};
+    p2={2,3};
+    cout<<(p1<p2)<<endl;
 }
