1c607ea6760fa779f9eb5d2a9aa8b308a6174f31
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-21 15:59:50 +0530

    added parenthesis code

--- CompetitiveCoding/e37-e46/parenthesis.cpp
+++ CompetitiveCoding/e37-e46/parenthesis.cpp
@@ -1,8 +1,24 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
+void generate(string s,int open,int close){
+	if(open==0 && close==0){
+		valid.push_back(s);
+		return;
+	}
+	if(open>0){
+		s.push_back('(');
+		generate(s,open-1,close);
+		s.pop_back();
+	}
+	if(close>0){
+		
+	}
+}
+
 int main(){
 	//generate parenthesis of n layer.
-	
+
 	return 0;
 }
