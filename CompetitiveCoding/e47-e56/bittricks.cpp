8455047ef2ae0e067580e4fa9d15e1a3ed803e6c
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-27 09:07:51 +0530

    update

--- CompetitiveCoding/e47-e56/bittricks.cpp
+++ CompetitiveCoding/e47-e56/bittricks.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 void printb(int num){
-    for(int i=15;i>=0;--i){
+    for(int i=10;i>=0;--i){
         cout<<((num>>i)&1);
     }
     cout<<endl;
@@ -22,21 +22,48 @@ void multiplyby2(int num){
     cout<<(num<<1)<<endl;
 }
 
-void upper(){
+void uppercase(char low){
+    cout<<char(low&(~(1<<5)))<<endl;
+}
+void lowercase(char upp){
+    char low=upp|(1<<5);
+    cout<<low<<endl;
+}
+void ClearLSBuptoi(int num,int i){
+    int result;
+    result=(num&(~((1<<(i+1))-1)));
+    cout<<result<<endl;
+    printb(result);
 
 }
-void lower(){
-
+void ClearMSBuptoi(int num,int i){
+    int result;
+    result=(num&((1<<(i+1))-1));
+    cout<<result<<endl;
+    printb(result);
 }
 int main()
 {
+    
+    cout<<44<<endl;
+    printb(44);
+    ClearLSBuptoi(59,4);
+    ClearMSBuptoi(59,4);
+    // char input;
+    // cin>>input;
+    // lowercase(input);
+    // char input2;
+    // cin>>input2;
+    // uppercase(input2);
+    // cout<<A<<a<<endl;
+    // cout<<char((a&(~(1<<5))))<<endl;
 
-    for(int i=1;i<=4;i++){
-        printb(i);
-        oddeven(i);
-    }
-    divideby2(10);
-    multiplyby2(10);
+    // for(int i=1;i<=4;i++){
+        // printb(i);
+        // oddeven(i);
+    // }
+    // divideby2(10);
+    // multiplyby2(10);
     // upper case and lowercase letters.
     // for(char c='A';c<='D';++c){
     //     cout<<c<<" ";
@@ -48,10 +75,7 @@ int main()
     //     cout<<int(c)<<" ";
     //     printb(int(c));
     // }
-    // char A='A';
-    // char a=A|(1<<5);
-    // cout<<A<<a<<endl;
-    // cout<<char((a&(~(1<<5))))<<endl;
+    
     // printb(59);                                                                                                                                                                                                                                 
     // int a=59;
     // int i=4;
