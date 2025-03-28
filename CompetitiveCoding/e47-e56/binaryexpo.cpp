b94fa0d63a3fed3095a77d8f86d298e5aeeff271
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-28 06:38:21 +0530

    binary expo

--- CompetitiveCoding/e47-e56/binaryexpo.cpp
+++ CompetitiveCoding/e47-e56/binaryexpo.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int M=1e9+7;
+
+int binExpRecur(int a, int b){
+	if(b==0) return 1;
+	int res=binExpRecur(a,b/2);
+	if(b&1){
+		return (a*((res*1LL*res)%M))%M;
+	}
+	else
+	{
+		//if b is even
+		return (res*1LL*res)%M;
+	}
+}
 int main()
 {
-    
+    int a=2; int b=13;
+    cout<<binExpRecur(a,b);
 }
--- CompetitiveCoding/e47-e56/expo.cpp
+++ CompetitiveCoding/e47-e56/expo.cpp
@@ -1,21 +1,6 @@
 #include<bits/stdc++.h>
 const int M=1e9+7;
 using namespace std;
-innt binExpRecur(int a,int b){
-    if(b==0)
-    {
-        return 1;
-    }
-    long long res=binExpRecur(a,b/2);
-    if(b&1){
-        //odd condition
-        return a*((res*res)%M)%M;
-    }
-    else
-    {   //even condition
-        return (res*res)%M;
-    }
-}
 int main()
 {
     int a=2;int b=13;
