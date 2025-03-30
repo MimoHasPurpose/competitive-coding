9490213db46e5b2819bb19d9642d20ccc9e697a3
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-30 16:01:35 +0530

    done without code

---
+++ CompetitiveCoding/e57-e68/inclusionExclusion.cpp
@@ -0,0 +1,6 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	// The inclusion-exclusion principle is an important combinatorial way to compute the size of a set or the probability of complex events. It relates the sizes of individual sets with their union.
+}
