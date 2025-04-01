14f005603c4f2c2ec85ee78f678af1b9023832da
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-04-01 11:24:03 +0530

    lcs and lcs dp
    wrote the code for lcs and lcs using dp in cpp

--- CompetitiveCoding/e79-e100/e90-last/LIS-memo.cpp
+++ CompetitiveCoding/e79-e100/e90-last/LIS-memo.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 const int N=25e2+10;
 vector<int> a(N);
-
+int dp[N];
 int lis( int i){
+	if(dp[i]!=-1) return dp[i];
 	int ans=1;
 	for(int j=0;j<i;++j){
 		if(a[i]>a[j]){
@@ -12,9 +13,10 @@ int lis( int i){
 		}
 
 	}
-	return ans;
+	return dp[i]=ans;
 }
 int main(){
+	memset(dp,-1,sizeof(dp));
 	int n;
 	cin>>n;
 	for(int i=0;i<=n;++i){
