b838eb53c02c2a192ac11e9a9a3680489e0caaf5
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-04-04 05:27:54 +0530

    knapsack

--- CompetitiveCoding/e79-e100/e90-last/knapsack2.cpp
+++ CompetitiveCoding/e79-e100/e90-last/knapsack2.cpp
@@ -1,7 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-	
-	return 0;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+class Solution {
+  public:
+   int dp[1005];
+    int func(int len, vector<int> &prices){
+        if(len==0)  return 0;
+        if(dp[len]!=-1) return dp[len];
+        int ans=0;
+        for(int len_to_cut=1;len_to_cut<=prices.size(); ++len_to_cut)
+        {
+            if(len-len_to_cut>=0){
+                ans=max(ans,func(len-len_to_cut,prices)+prices[len_to_cut-1]);
+            }
+        }
+        return dp[len]=ans;
+    }
+  
+    int cutRod(vector<int> &price) {
+        // code here
+        memset(dp,-1,sizeof(dp));
+        return func(price.size(),price);
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution ob;
+
+        cout << ob.cutRod(a) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
 }
+// } Driver Code Ends
