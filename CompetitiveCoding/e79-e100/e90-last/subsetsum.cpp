c8449a3a1eb7948dc39317cd076a903b44084839
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-04-04 06:17:14 +0530

    subset sum

--- CompetitiveCoding/e79-e100/e90-last/subsetsum.cpp
+++ CompetitiveCoding/e79-e100/e90-last/subsetsum.cpp
@@ -1,6 +1,28 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+bool func(int i,int sum,vector<int> &nums){
+
+	if(sum==0) return true;
+	if(i<0) return false;
+	//not consider ith index.
+
+	func(i-1,sum);
+
+	//consider ith index
+	func(i-1,sum-nums[i]);
+	return isPossible;
+
+}
+
+ bool canPartition(vector<int>&nums){
+ 	int sum=accumulate(nums.begin(),nums.end(),0);
+ 	if(sum%2!=0){
+ 		return false;
+ 	}
+ 	sum=sum/2;
+ 	return func(num.size()-1,sum,nums);
+ }
 int main(){
 	
 	return 0;
